d4ad98a2e60911d4f04c95e4e98280013889a853
kon3gor <bboymishka888@gmail.com>
2020-03-20 11:03:42 +0300

    py version has been removed due to new py version

--- python/values.py
+++
@@ -1,8 +0,0 @@
-"""
-This is frequently used values for the API.
-"""
-MAX_X       =  8000000                                                                   # Max X value for slide
-MAX_Y       =  5000000                                                                   # Max Y value for slide
-SCOPES      =  ['https://www.googleapis.com/auth/presentations']                         # Scopes for the Slides API
-baseImgUrl  =  "https://raw.githubusercontent.com/kon3gor/SlidesCatizer/master/images/"  # Common part of the URL for photos in the github repo
-githubToken =  ""                                                                        # Github token to access Github graphql API
